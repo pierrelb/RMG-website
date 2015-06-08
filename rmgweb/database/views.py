1338ac1910a59eabaadb0efb88c9d9070fb421cf
Pierre Bhoorasingh <bhoorasingh.p@husky.neu.edu>
2015-06-08 11:13:27 -0400

    Update imports to use BeautifulSoup4 while still allowing older versions
    Import for BeautifulSoup 4 is now `from bs4 import BeautifulSoup`

--- rmgweb/database/views.py
+++ rmgweb/database/views.py
@@ -41,7 +41,10 @@ import time
 import urllib
 import urllib2
 
-from BeautifulSoup import BeautifulSoup
+try:
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import Http404, HttpResponseRedirect, HttpResponse
